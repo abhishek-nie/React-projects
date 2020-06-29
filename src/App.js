22eb6568e525d808c998ac70b0aa9e2977dfbe04
abhishek-nie <48669065+abhishek-nie@users.noreply.github.com>
2020-06-29 14:08:36 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,40 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+const api = {
+  key: "7d4ef5d23648c7c62acd3ccfa1a0a30b",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
-import Cards from './components/Cards/Cards';
-import CountryPicker from './components/CountryPicker/CountryPicker';
-import Chart from './components/Chart/Chart';
-import {fetchData} from './api';
-import coronaImage from './image/image.png';
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
-import styles from './App.module.css';
-
-class App extends React.Component {
-  state = {
-      data : {},
-      country: '',
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
   }
 
-  async componentDidMount(){
-        const fetchedData = await fetchData();
-        this.setState({data : fetchedData})
-    }
-    handleCountryChange = async (country)=>{
-        const fetchedData = await fetchData(country);
-        this.setState({data : fetchedData, country :country})
-    }
-    render() {
-        const {data,country} = this.state;
-        return (
-            <div className={styles.container}>
-                <img className={styles.image} src={coronaImage} alt="COVID-19"></img>
-            <Cards data={data} />
-            <CountryPicker handleCountryChange={this.handleCountryChange}/>
-            <Chart data={data} country ={country}/>
-            
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-        )
-        
-    }
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
+    </div>
+  );
 }
 
 export default App;
